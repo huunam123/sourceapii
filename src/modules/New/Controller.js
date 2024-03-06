59fc3c029a0f0e8d9b8fe33249a26a549d82a608
huunam123 <huunam123@example.com>
2024-03-06 13:47:25 +0700

    complete uload image in client

--- src/modules/New/Controller.js
+++ src/modules/New/Controller.js
@@ -7,128 +7,204 @@ const path = require('path');
 // const uploadDirectory = path.join(__dirname, 'src', 'upload', 'news_images');
  //const mkdirSync = fs.mkdirSync;
 
-        module.exports = class extends Controller {
-                constructor(tableName) {
-                    super(tableName);
-                    this.db = new Model('news');
-                } 
+module.exports = class extends Controller {
+    constructor(tableName) {
+        super(tableName);
+        this.db = new Model('news');
+    } 
 
-                async create(req, res) {
+    async create(req, res) {
+        try {
+            // Validate
+            const _check = await this.validate(req, res);
+
+            if (_check == false) {
+                let _data = req.body;
+                _data.created_at = new Date();
+                _data.updated_at = new Date();
+
+                if (_data?.image) {
                     try {
-                        // Validate
-                        const _check = await this.validate(req, res);
+                        const _year = this.moment().format('YYYY');
+                        const _month = this.moment().format('MM');
+                        const _parse = extractBase64(_data.image.file);
 
-                        if (_check == false) {
-                            let _data = req.body;
-                            _data.created_at = new Date();
-                            _data.updated_at = new Date();
-
-                            if (_data?.image) {
-                                try {
-                                    const _year = this.moment().format('YYYY');
-                                    const _month = this.moment().format('MM');
-
-                                    const _parse = extractBase64(_data.image.file);
-
-                                    if (!_parse) {
-                                        return this.response(res, 500, 'Failed to parse image data');
-                                    }
-
-                                    const _name = `/${nanoid()}.${_parse.ext}`;
-
-                                    //const imagePath = path.join(process.env.DIR_UPLOAD, 'src/upload/user_images/images_local', _name);
-                                const imagePath = path.join('src/upload/news_images', _name);  
-                                        console.log(imagePath)
-
-                                    await this.uploadFileLocal(_data.image,imagePath);
-                                    _data.image = _name;
-                                } catch (e) {
-                                    console.error(e);
-                                    return this.response(res, 500, 'Upload failure, please try again');
-                                }
-                            }
-
-                            await this.db.insert(_data);
-                            return this.response(res, 201);
+                        if (!_parse) {
+                            return this.response(res, 500, 'Failed to parse image data');
                         }
+
+                        const _name = `/${nanoid()}.${_parse.ext}`;
+                        const imagePath = path.join('/public/upload/news_images', _name); 
+                        await this.uploadFileLocal(_data.image,imagePath);
+                        _data.image = imagePath;
                     } catch (e) {
                         console.error(e);
-                        return this.response(res, 500, e.message);
+                        return this.response(res, 500, 'Upload failure, please try again');
                     }
                 }
 
+                await this.db.insert(_data);
+                return this.response(res, 201);
+            }
+        }
+        catch (e) {
+            console.error(e);
+            return this.response(res, 500, e.message);
+        }
+    }
 
+    async  uploadFileLocal(imageData,imagePath) {
+        try {
+            if (imageData && imagePath) {
+                const base64Data = imageData.file.replace(/^data:image\/\w+;base64,/, '');
+                const _dir =  "D:\\NETDEV\\mcv source baitap\\sourceMCCVV\\source\\sourceapii\\public\\upload\\news_images" || path.resolve(process.env.DIR_UPLOAD, 'news_images');// đường dẫn lưu trữ ảnh 
+                
+                if (!fs.existsSync(_dir)) fs.mkdirSync(_dir);
+                
+                fs.writeFileSync(path.join('D:\\NETDEV\\mcv source baitap\\sourceMCCVV\\source\\sourceapii\\', imagePath), base64Data, 'base64', function(err) {  
+                    if(err) console.error(err);
+                    else console.log('File created successfully.');
+                });
 
-                //upload image 
-               
-                async uploadFileLocal(imageData,imagePath) {
+                return path.join(_dir, imagePath);
+                
+            } else {
+                console.error('Image data is missing or invalid');
+                return null;
 
-                    console.log(3333, imageData,imagePath)
-
-                    try {
-                        if (imageData && imagePath) {
-                         
-                            const base64Data = imageData.file.replace(/^data:image\/\w+;base64,/, '');
-
-                           // const _dir =  "D:\\NETDEV\\mcv source baitap\\sourceMCCVV\\source\\sourceapii\\src\\upload" || path.resolve(process.env.DIR_UPLOAD);
-             const _dir =  "D:\\NETDEV\\mcv source baitap\\sourceMCCVV\\source\\sourceapii\\src\\upload\\news_images" || path.resolve(process.env.DIR_UPLOAD, 'news_images');
-                         // const _dir đường dẫn đã tạo ra file cần chỉnh sửa cho gọn
-                            
-                     //mẫu     //  if (!fs.existsSync(_dir)) fs.mkdirSync(_dir); 
-
-                     //đã fix 
-                          if (!fs.existsSync(_dir)) {
-                            fs.mkdirSync(_dir);
-                        }
-
-
-       // giải thích //fs.existsSync  Phương thức fs.existsSync() được sử dụng để kiểm tra đồng bộ xem tệp đã tồn tại trong đường dẫn nhất định hay chưa. 
-                           
-                    function generateUniqueFilename(fileName) {
-                        const timestamp = Date.now();
-                        const extension = path.extname(fileName);
-                        const nameWithoutExtension = path.basename(fileName, extension);
-                        return `${nameWithoutExtension}_${timestamp}${extension}`;
-                    }
-                    
-                    const fileName = "image.jpg";
-                    const uniqueFileName = generateUniqueFilename(fileName);
-
-                    fs.writeFileSync(path.join(_dir, uniqueFileName), base64Data, 'base64', function(err) { 
-                        if(err) {
-                            console.error(err);
-                        } else {
-                            console.log('File created successfully.');
-                        }
-                    });
-                    
-                           //(!fs.existsSync(_dir+'/news_images')) fs.mkdirSync(_dir+'/news_images');   //lỗi 
-                          
-                           
-                        //  1  const fileName = "image.jpg"; 
-                        //    fs.writeFileSync(path.join(_dir, fileName), base64Data, 'base64', function(err) { 
-                        //        if(err) {
-                        //            console.error(err);
-                        //        } else {
-                        //            console.log('File created successfully.');
-                        //        }
-                        //    });
-
-                          //1 fs.writeFileSync(_dir + '/'+ "fileName", base64Data, 'base64', function(err) { console.log(err);}); // fixx chỗ _name là text chứ hông phải hình
-                          //2 return true;
-                         
-                            
-                        } else {
-                            console.error('Image data is missing or invalid');
-                            return null;
-
-                        }
-                    } catch (e) {
-                        console.error(e);
-                        return e.message;
-                    }
-                }
+            }
+        } catch (e) {
+            console.error(e);
+            return e.message;
+        }
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
+//source cũ
+
+// const Controller = require("@system/Controller");
+// const Model = require('@system/Model');
+// const { extractBase64 } = require("@utils/Helper");
+// const { nanoid } = require("nanoid");
+// const fs = require('fs');
+// const path = require('path');
+
+
+
+// module.exports = class extends Controller {
+//     constructor(tableName) {
+//         super(tableName);
+//         this.db = new Model('news');
+//     } 
+
+//  async create(req, res) {
+//         try {
+//             // Validate
+//             const _check = await this.validate(req, res);
+
+//  if (_check == false) {
+//                 let _data = req.body;
+//                 _data.created_at = new Date();
+//                 _data.updated_at = new Date();
+
+//                 if (_data?.image) {
+//                     try {
+//                         const _year = this.moment().format('YYYY');
+//                         const _month = this.moment().format('MM');
+
+//                         const _parse = extractBase64(_data.image.file);
+
+//                         if (!_parse) {
+//                             return this.response(res, 500, 'Failed to parse image data');
+//                         }
+
+//                         const _name = `/${nanoid()}.${_parse.ext}`;
+
+//                         //const imagePath = path.join(process.env.DIR_UPLOAD, 'src/upload/user_images/images_local', _name);
+//                      const imagePath = path.join('src/upload/news_images', _name);  
+//                             console.log(imagePath)
+
+//                         await this.uploadFileLocal(_data.image,imagePath);
+//                         _data.image = _name;
+//                     } catch (e) {
+//                         console.error(e);
+//                         return this.response(res, 500, 'Upload failure, please try again');
+//                     }
+//                 }
+
+
+//    await this.db.insert(_data);
+//                 return this.response(res, 201);
+//             }
+//         } catch (e) {
+//             console.error(e);
+//             return this.response(res, 500, e.message);
+//         }
+//     }
+
+
+
+
+//  //upload image 
+               
+//                 async uploadFileLocal(imageData,imagePath) {
+
+//                     console.log(3333, imageData,imagePath)
+
+//                     try {
+//                         if (imageData && imagePath) {
+                         
+//                             const base64Data = imageData.file.replace(/^data:image\/\w+;base64,/, '');
+//                             //console.log(11111,base64Data,process.env.DIR_UPLOAD)
+
+//                             const _dir =  "D:\\NETDEV\\mcv source baitap\\sourceMCCVV\\source\\sourceapii\\src\\upload" || path.resolve(process.env.DIR_UPLOAD);
+                           
+                            
+
+
+//                             if (!fs.existsSync(_dir)) fs.mkdirSync(_dir); //fix laij 
+//                             //  if (!fs.existsSync(uploadDirectory)) {
+//                             //     fs.mkdirSync(uploadDirectory, { recursive: true });
+
+//                             //     mkdirSync(_dir, { recursive: true });
+//                             // }
+
+//                            // console.log (555555,!fs.existsSync(_dir))
+//                             // console.log(mkdirSyhonc,_dir)
+//                             // mkdirSync(_dir, { recursive: true });
+
+//                             //if (!fs.existsSync(imageDir)) fs.mkdirSync(imageDir);
+
+                    
+//                            //(!fs.existsSync(_dir+'/news_images')) fs.mkdirSync(_dir+'/news_images');
+                          
+//                            fs.writeFileSync(_dir+'/'+"bbbb", base64Data, 'base64', function(err) { console.log(err);});
+//                            return true;
+//                             //console.log(55555,imagePath,)
+                           
+//                         } else {
+//                             console.error('Image data is missing or invalid');
+//                             return null;
+
+//                         }
+//                     } catch (e) {
+//                         console.error(e);
+//                         return e.message;
+//                     }
+//                 }
+// };
+
+
 
 
 

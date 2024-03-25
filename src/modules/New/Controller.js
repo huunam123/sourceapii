8fc3cf3f0399eec09e49e1a1251fc4da508da6d2
huunam123 <huunam123@example.com>
2024-03-25 10:25:34 +0700

    22+24/3

--- src/modules/New/Controller.js
+++ src/modules/New/Controller.js
@@ -94,7 +94,7 @@ module.exports = class extends Controller {
         try {
             if (imageData && imagePath) {
                 const base64Data = imageData.file.replace(/^data:image\/\w+;base64,/, '');
-                const _dir =  "D:\\NETDEV\\mcv source baitap\\sourceMCCVV\\source\\sourceapii\\public\\upload\\news_images" || path.resolve(process.env.DIR_UPLOAD, 'news_images');// đường dẫn lưu trữ ảnh 
+                const _dir =  "D:\\NETDEV\\mcv source baitap\\sourceMCCVV\\source\\sourceapii\\public\\upload\\news_images" || path.resolve(process.env.DIR_UPLOAD, 'news_images');
                 
                 if (!fs.existsSync(_dir)) fs.mkdirSync(_dir);
                 
@@ -118,128 +118,6 @@ module.exports = class extends Controller {
 };
 
 
-
-
-
-
-
-
-
-
-
-//source cũ
-
-// const Controller = require("@system/Controller");
-// const Model = require('@system/Model');
-// const { extractBase64 } = require("@utils/Helper");
-// const { nanoid } = require("nanoid");
-// const fs = require('fs');
-// const path = require('path');
-
-
-
-// module.exports = class extends Controller {
-//     constructor(tableName) {
-//         super(tableName);
-//         this.db = new Model('news');
-//     } 
-
-//  async create(req, res) {
-//         try {
-//             // Validate
-//             const _check = await this.validate(req, res);
-
-//  if (_check == false) {
-//                 let _data = req.body;
-//                 _data.created_at = new Date();
-//                 _data.updated_at = new Date();
-
-//                 if (_data?.image) {
-//                     try {
-//                         const _year = this.moment().format('YYYY');
-//                         const _month = this.moment().format('MM');
-
-//                         const _parse = extractBase64(_data.image.file);
-
-//                         if (!_parse) {
-//                             return this.response(res, 500, 'Failed to parse image data');
-//                         }
-
-//                         const _name = `/${nanoid()}.${_parse.ext}`;
-
-//                         //const imagePath = path.join(process.env.DIR_UPLOAD, 'src/upload/user_images/images_local', _name);
-//                      const imagePath = path.join('src/upload/news_images', _name);  
-//                             console.log(imagePath)
-
-//                         await this.uploadFileLocal(_data.image,imagePath);
-//                         _data.image = _name;
-//                     } catch (e) {
-//                         console.error(e);
-//                         return this.response(res, 500, 'Upload failure, please try again');
-//                     }
-//                 }
-
-
-//    await this.db.insert(_data);
-//                 return this.response(res, 201);
-//             }
-//         } catch (e) {
-//             console.error(e);
-//             return this.response(res, 500, e.message);
-//         }
-//     }
-
-
-
-
-//  //upload image 
-               
-//                 async uploadFileLocal(imageData,imagePath) {
-
-//                     console.log(3333, imageData,imagePath)
-
-//                     try {
-//                         if (imageData && imagePath) {
-                         
-//                             const base64Data = imageData.file.replace(/^data:image\/\w+;base64,/, '');
-//                             //console.log(11111,base64Data,process.env.DIR_UPLOAD)
-
-//                             const _dir =  "D:\\NETDEV\\mcv source baitap\\sourceMCCVV\\source\\sourceapii\\src\\upload" || path.resolve(process.env.DIR_UPLOAD);
-                           
-                            
-
-
-//                             if (!fs.existsSync(_dir)) fs.mkdirSync(_dir); //fix laij 
-//                             //  if (!fs.existsSync(uploadDirectory)) {
-//                             //     fs.mkdirSync(uploadDirectory, { recursive: true });
-
-//                             //     mkdirSync(_dir, { recursive: true });
-//                             // }
-
-//                            // console.log (555555,!fs.existsSync(_dir))
-//                             // console.log(mkdirSyhonc,_dir)
-//                             // mkdirSync(_dir, { recursive: true });
-
-//                             //if (!fs.existsSync(imageDir)) fs.mkdirSync(imageDir);
-
-                    
-//                            //(!fs.existsSync(_dir+'/news_images')) fs.mkdirSync(_dir+'/news_images');
-                          
-//                            fs.writeFileSync(_dir+'/'+"bbbb", base64Data, 'base64', function(err) { console.log(err);});
-//                            return true;
-//                             //console.log(55555,imagePath,)
-                           
-//                         } else {
-//                             console.error('Image data is missing or invalid');
-//                             return null;
-
-//                         }
-//                     } catch (e) {
-//                         console.error(e);
-//                         return e.message;
-//                     }
-//                 }
-// };
 
 
 
